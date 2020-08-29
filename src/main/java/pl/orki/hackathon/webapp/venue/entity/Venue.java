bf8c77ea0bfc1573cde298c1f5941101de1f609e
Paweł Oczadly <oczadlypawe@gmail.com>
2020-08-29 16:03:33 +0200

    Added venue model (#1)

---
+++ src/main/java/pl/orki/hackathon/webapp/venue/entity/Venue.java
@@ -0,0 +1,117 @@
+package pl.orki.hackathon.webapp.venue.entity;
+
+import pl.orki.hackathon.webapp.city.City;
+import pl.orki.hackathon.webapp.genre.MusicGenre;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.Set;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "venue")
+public class Venue {
+
+    @Id
+    @GeneratedValue(generator = "venue_generator")
+    @SequenceGenerator(
+            name = "venue_generator",
+            sequenceName = "venue_sequence"
+    )
+    @Column(name = "venue_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "city")
+    private City city;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "music_genre")
+    @ElementCollection(targetClass = MusicGenre.class)
+    @CollectionTable(name="venue_genre")
+    private Set<MusicGenre> musicGenre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Set<MusicGenre> getMusicGenre() {
+        return musicGenre;
+    }
+
+    public void setMusicGenre(Set<MusicGenre> musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Venue venue = (Venue) o;
+        return Objects.equals(id, venue.id) &&
+                Objects.equals(name, venue.name) &&
+                Objects.equals(capacity, venue.capacity) &&
+                Objects.equals(city, venue.city) &&
+                Objects.equals(musicGenre, venue.musicGenre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, capacity, city, musicGenre);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Venue.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("capacity=" + capacity)
+                .add("city=" + city)
+                .add("musicGenre=" + musicGenre)
+                .toString();
+    }
+}
---
+++ src/main/java/pl/orki/hackathon/webapp/venue/entity/VenueRepository.java
@@ -0,0 +1,6 @@
+package pl.orki.hackathon.webapp.venue.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+}
