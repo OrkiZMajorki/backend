b07054673a9d07f0e1c3fb359d52e09e0f81fe91
JakubKowalski1997 <kubabartek1@op.pl>
2020-08-30 07:50:37 +0200

    fix findByGenreAndCity

--- src/main/java/pl/orki/hackathon/webapp/band/boundary/BandQuery.java
+++ src/main/java/pl/orki/hackathon/webapp/band/boundary/BandQuery.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import pl.orki.hackathon.webapp.band.entity.BandRepository;
+import pl.orki.hackathon.webapp.city.City;
+import pl.orki.hackathon.webapp.genre.MusicGenre;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -21,8 +23,8 @@ public class BandQuery implements GraphQLQueryResolver {
     @Transactional(readOnly = true)
     public Set<BandDTO> findBandsByGenreAndCity(Set<String> genres, Set<String> cities) {
         return bandRepository.findAll().stream()
-                .filter(band -> CollectionUtils.containsAny(genres, band.getMusicGenres()))
-                .filter(band -> CollectionUtils.containsAny(cities, band.getCities()))
+                .filter(band -> CollectionUtils.containsAny(genres, band.getMusicGenres().stream().map(MusicGenre::toString).collect(Collectors.toSet())))
+                .filter(band -> CollectionUtils.containsAny(cities, band.getCities().stream().map(City::toString).collect(Collectors.toSet())))
                 .map(BandConverter::convertToDTO)
                 .collect(Collectors.toSet());
     }
