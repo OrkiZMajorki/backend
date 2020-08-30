511eb5dbcdbbbc5fd83b86b9018ba921f5a4a02e
Pawlllosss <oczadlypawe@gmail.com>
2020-08-30 07:37:06 +0200

    create band with attached user mutation

--- src/main/java/pl/orki/hackathon/webapp/band/control/BandService.java
+++ src/main/java/pl/orki/hackathon/webapp/band/control/BandService.java
@@ -3,20 +3,28 @@ package pl.orki.hackathon.webapp.band.control;
 import org.springframework.stereotype.Service;
 import pl.orki.hackathon.webapp.band.entity.Band;
 import pl.orki.hackathon.webapp.band.entity.BandRepository;
+import pl.orki.hackathon.webapp.user.entity.UserRepository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class BandService {
 
     private final BandRepository bandRepository;
+    private final UserRepository userRepository;
 
-    public BandService(BandRepository bandRepository) {
+    public BandService(BandRepository bandRepository, UserRepository userRepository) {
         this.bandRepository = bandRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
-    public Band createBand(Band band) {
-        return bandRepository.save(band);
+    public Optional<Band> createBand(Band band, Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    band.setUser(user);
+                    return bandRepository.save(band);
+                });
     }
 }
