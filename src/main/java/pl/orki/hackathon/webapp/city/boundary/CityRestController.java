c560ab2fa5b22cce28e6c3393488ee680150b163
Pawlllosss <oczadlypawe@gmail.com>
2020-10-01 22:23:27 +0200

    fixed CityRestController

--- src/main/java/pl/orki/hackathon/webapp/city/boundary/CityRestController.java
+++ src/main/java/pl/orki/hackathon/webapp/city/boundary/CityRestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.orki.hackathon.webapp.city.boundary.dto.CityConverter;
 import pl.orki.hackathon.webapp.city.boundary.dto.CityResponseDTO;
 import pl.orki.hackathon.webapp.city.control.CityService;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@RestController
 @RequestMapping("/city")
 public class CityRestController {
 
